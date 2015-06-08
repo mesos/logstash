cb9ca20658b94aa1583e912445212168986745ef
Emil Rondahl <ero@trifork.com>
2015-06-08 14:57:29 +0200

    Adds first impl of LogstashScheduler main

--- scheduler/src/main/java/org/apache/mesos/logstash/scheduler/LogstashScheduler.java
+++ scheduler/src/main/java/org/apache/mesos/logstash/scheduler/LogstashScheduler.java
@@ -1,5 +1,12 @@
 package org.apache.mesos.logstash.scheduler;
 
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.log4j.Logger;
 import org.apache.mesos.MesosSchedulerDriver;
 import org.apache.mesos.Protos;
 import org.apache.mesos.Protos.ContainerInfo.DockerInfo.PortMapping;
@@ -13,12 +20,71 @@ import java.util.List;
  */
 public class LogstashScheduler implements Scheduler, Runnable {
 
-    public static void main(String[] args) {
+    public static final Logger LOGGER = Logger.getLogger(LogstashScheduler.class.toString());
+    private static final int MESOS_PORT = 5050;
+    private static final String FRAMEWORK_NAME = "LOGSTASH";
 
+    private String master;
+
+    // As per the DCOS Service Specification, setting the failover timeout to a large value;
+    private static final double FAILOVER_TIMEOUT = 86400000;
+
+    public LogstashScheduler(String master) {
+        this.master = master;
+    }
+
+    public static void main(String[] args) {
+        Options options = new Options();
+        options.addOption("m", "master host or IP", true, "master host or IP");
+        CommandLineParser parser = new BasicParser();
+        try {
+            CommandLine cmd = parser.parse(options, args);
+            String masterHost = cmd.getOptionValue("m");
+            if (masterHost == null) {
+                printUsage(options);
+                return;
+            }
+
+            LOGGER.info("Starting ElasticSearch on Mesos");
+            final LogstashScheduler scheduler = new LogstashScheduler(masterHost);
+
+            final Protos.FrameworkInfo.Builder frameworkBuilder = Protos.FrameworkInfo.newBuilder();
+            frameworkBuilder.setUser("jclouds");
+            frameworkBuilder.setName(FRAMEWORK_NAME);
+            frameworkBuilder.setCheckpoint(true);
+            frameworkBuilder.setFailoverTimeout(FAILOVER_TIMEOUT);
+
+            final MesosSchedulerDriver driver = new MesosSchedulerDriver(scheduler, frameworkBuilder.build(), masterHost + ":" + MESOS_PORT);
+
+            Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    driver.stop();
+                    scheduler.onShutdown();
+                }
+            }));
+
+            Thread schedThred = new Thread(scheduler);
+            schedThred.start();
+        } catch (ParseException e) {
+            printUsage(options);
+        }
+    }
+
+    private void onShutdown() {
+        LOGGER.info("On shutdown...");
+    }
+
+    private static void printUsage(Options options) {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp(FRAMEWORK_NAME, options);
     }
 
     @Override
     public void run() {
+        LOGGER.info("Starting up ...");
+        SchedulerDriver driver = new MesosSchedulerDriver(this, Protos.FrameworkInfo.newBuilder().setUser("").setName(FRAMEWORK_NAME).build(), master + ":" + MESOS_PORT);
+        driver.run();
 
     }
 
