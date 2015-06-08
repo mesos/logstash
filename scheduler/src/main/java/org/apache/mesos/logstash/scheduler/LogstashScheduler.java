98a838a96cf9f243f85c10b522ef8d50ef3ff151
Emil Rondahl <ero@trifork.com>
2015-06-08 13:51:18 +0200

    Project structure

---
+++ scheduler/src/main/java/org/apache/mesos/logstash/scheduler/LogstashScheduler.java
@@ -0,0 +1,74 @@
+package org.apache.mesos.logstash.scheduler;
+
+import org.apache.mesos.MesosSchedulerDriver;
+import org.apache.mesos.Protos;
+import org.apache.mesos.Protos.ContainerInfo.DockerInfo.PortMapping;
+import org.apache.mesos.Scheduler;
+import org.apache.mesos.SchedulerDriver;
+
+import java.util.List;
+
+/**
+ * Created by ero on 08/06/15.
+ */
+public class LogstashScheduler implements Scheduler, Runnable {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void registered(SchedulerDriver schedulerDriver, Protos.FrameworkID frameworkID, Protos.MasterInfo masterInfo) {
+
+    }
+
+    @Override
+    public void reregistered(SchedulerDriver schedulerDriver, Protos.MasterInfo masterInfo) {
+
+    }
+
+    @Override
+    public void resourceOffers(SchedulerDriver schedulerDriver, List<Protos.Offer> list) {
+
+    }
+
+    @Override
+    public void offerRescinded(SchedulerDriver schedulerDriver, Protos.OfferID offerID) {
+
+    }
+
+    @Override
+    public void statusUpdate(SchedulerDriver schedulerDriver, Protos.TaskStatus taskStatus) {
+
+    }
+
+    @Override
+    public void frameworkMessage(SchedulerDriver schedulerDriver, Protos.ExecutorID executorID, Protos.SlaveID slaveID, byte[] bytes) {
+
+    }
+
+    @Override
+    public void disconnected(SchedulerDriver schedulerDriver) {
+
+    }
+
+    @Override
+    public void slaveLost(SchedulerDriver schedulerDriver, Protos.SlaveID slaveID) {
+
+    }
+
+    @Override
+    public void executorLost(SchedulerDriver schedulerDriver, Protos.ExecutorID executorID, Protos.SlaveID slaveID, int i) {
+
+    }
+
+    @Override
+    public void error(SchedulerDriver schedulerDriver, String s) {
+
+    }
+}
