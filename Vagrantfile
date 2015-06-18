cb989a4801dccd4d334cd3a23429ea76760435cc
Emil Rondahl <ero@trifork.com>
2015-06-18 09:49:35 +0200

    Adjusting vagrant to run with ubuntu

--- Vagrantfile
+++ Vagrantfile
@@ -1,11 +1,10 @@
 Vagrant.configure('2') do |config|
 
   # Every Vagrant virtual environment requires a box to build off of.
-  config.vm.box = 'centos70'
+  config.vm.box = 'ubuntu/trusty64'
   
   config.ssh.forward_agent = true
 
-  config.vm.box_url = 'http://deltaforce.io/box/centos70-base_1414068169.box'
   config.vm.network "private_network", ip: "192.168.50.10"
   config.vm.provider :virtualbox do |vb|
     vb.customize ['modifyvm', :id, '--memory', 2048]
--- vagrant_files/setup.sh
+++ vagrant_files/setup.sh
@@ -1,59 +1,41 @@
 #!/bin/bash -e
 
+apt-get -y update
+apt-get -y install linux-image-generic-lts-trusty
+sudo apt-get install wget
+
 # Docker setup
 echo "Installing docker"
-yum install -y docker
-
-echo "Disabling SELinux as it causes some issues with BTRFS and we don't need it in development anyway"
-if grep -Pq "^SELINUX=(enforcing|permissive)" /etc/selinux/config ; then
-        sed -ri 's:^SELINUX=(enforcing|permissive):SELINUX=disabled:' /etc/selinux/config
-fi
-if grep -Pq "^SELINUX=(enforcing|permissive)" /etc/sysconfig/selinux ; then
-	sed -ri 's:^SELINUX=(enforcing|permissive):SELINUX=disabled:' /etc/sysconfig/selinux
-fi
-setenforce 0
-echo "SELINUX is now disabled"
+wget -qO- https://get.docker.com/ | sh
 
 echo "Updating docker config to ignore SELinux and to accept http"
-cp -f /vagrant/vagrant_files/etc/sysconfig/docker.new /etc/sysconfig/docker
+cp -f /vagrant/vagrant_files/etc/sysconfig/docker.new /etc/default/docker
 
 echo "Adding env vars in profile.d"
 cp -f /vagrant/vagrant_files/etc/profile.d/env_vars.sh /etc/profile.d/
 
 echo "Restarting docker"
-systemctl restart docker
+service docker restart
 
 echo "Installing docker-compose"
 curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/bin/docker-compose
 chmod +x /usr/bin/docker-compose
 
-echo "Pulling docker registry image"
-docker pull registry:2.0
-
 echo "Installing jdk"
-yum install -y java-1.8.0-openjdk-devel.x86_64
+apt-get -y install default-jdk
 
 echo "Installing mesos"
 
-# According to the guide from mesosphere https://docs.mesosphere.com/tutorials/install_centos_rhel
-curl -sSfL http://archive.cloudera.com/cdh4/one-click-install/redhat/6/x86_64/cloudera-cdh-4-0.x86_64.rpm --output /tmp/cdh.rpm
-curl -sSfL http://archive.cloudera.com/cdh4/redhat/6/x86_64/cdh/RPM-GPG-KEY-cloudera --output /tmp/cdh.key
-rpm --import /tmp/cdh.key
-yum localinstall -y -q /tmp/cdh.rpm
+# According to the guide from mesosphere https://docs.mesosphere.com/tutorials/install_ubuntu_debian/#overview
+apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
+DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
+CODENAME=$(lsb_release -cs)
+echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | tee /etc/apt/sources.list.d/mesosphere.list
+apt-get -y update
+apt-get -y install mesos marathon
 
 echo "Installing zookeeper"
-yum install -y zookeeper-server rpm python-setuptools
-service zookeeper-server init
+apt-get install -y zookeeperd python-setuptools
 echo 1 | tee -a /var/lib/zookeeper/myid >/dev/null
-
-echo "Installing mesos"
-rpm -Uvh http://repos.mesosphere.io/el/7/noarch/RPMS/mesosphere-el-repo-7-1.noarch.rpm
-yum -y install mesos
-
-echo "Turn off firewall"
-systemctl disable firewalld
-
-echo "Starting a registry container"
-docker run -p 5000:5000 registry:2.0 &
 
 echo "Done!"
