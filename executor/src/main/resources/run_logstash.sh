4175cb3805fd5c7354ce2ce9e6fac53abffb152e
Erik Peldan <epe@trifork.com>
2015-06-24 12:03:18 +0200

    Write bash script to reload logstash when configuration changes

---
+++ executor/src/main/resources/run_logstash.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+SCRIPT_LOCATION=/tmp/logstash.conf
+TEMP_SCRIPT_LOCATION=/tmp/logstash.temp.conf
+
+cp $SCRIPT_LOCATION $TEMP_SCRIPT_LOCATION
+logstash -f $TEMP_SCRIPT_LOCATION &
+
+LOGSTASH_PID=$!
+echo "PID $LOGSTASH_PID"
+
+function stop() {
+  echo "Killing logstash $LOGSTASH_PID"
+  kill $LOGSTASH_PID
+  exit
+}
+
+trap stop SIGINT
+
+while :
+do
+  touch $TEMP_SCRIPT_LOCATION
+  diff $SCRIPT_LOCATION $TEMP_SCRIPT_LOCATION > /dev/null
+  if [ $? -ne 0 ]; then
+    echo "Config changed. Redeploying.."
+    cp $SCRIPT_LOCATION $TEMP_SCRIPT_LOCATION
+
+    echo "Killing logstash $LOGSTASH_PID"
+    kill $LOGSTASH_PID
+
+    echo "Restarting.."
+    logstash -f $TEMP_SCRIPT_LOCATION &
+    LOGSTASH_PID=$!
+    echo "PID $LOGSTASH_PID"
+  fi
+  sleep 1
+done
