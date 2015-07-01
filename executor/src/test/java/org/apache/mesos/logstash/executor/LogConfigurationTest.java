1b49fa7257f8235933c9c50430c9ea5ddcbd742d
Emil Rondahl <ero@trifork.com>
2015-07-01 10:27:59 +0200

    Fixing so gradle finds the locaton of local.properties

---
+++ executor/src/test/java/org/apache/mesos/logstash/executor/LogConfigurationTest.java
@@ -0,0 +1,16 @@
+package org.apache.mesos.logstash.executor;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ero on 01/07/15.
+ */
+public class LogConfigurationTest {
+
+    @Test
+    public void testSetLocalLogLocation() throws Exception {
+
+    }
+}
