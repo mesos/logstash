da23d3aeb4c6ecb6bbea22d92fee20634165918a
Erik Peldan <epe@trifork.com>
2015-07-09 11:00:07 +0200

    WIP test using mini-mesos

--- system-test/src/test/java/org/apache/mesos/logstash/systemtest/MessageTest.java
+++ system-test/src/test/java/org/apache/mesos/logstash/systemtest/MessageTest.java
@@ -1,13 +1,11 @@
 package org.apache.mesos.logstash.systemtest;
 
-import com.github.dockerjava.api.DockerClient;
 import org.apache.mesos.logstash.scheduler.Scheduler;
 import org.apache.mesos.mini.MesosCluster;
 import org.apache.mesos.mini.MesosClusterConfig;
-import org.apache.mesos.mini.util.DockerUtil;
+import org.apache.mesos.mini.state.Framework;
+import org.apache.mesos.mini.state.State;
 import org.junit.*;
-import org.junit.rules.ExternalResource;
-import org.junit.rules.RuleChain;
 
 import java.io.File;
 
@@ -16,37 +14,21 @@ import java.io.File;
  */
 public class MessageTest {
 
-    public static class SmallInnerClass extends ExternalResource {
-        // For usage as JUnit rule...
-        @Override
-        protected void before() throws Throwable {
-            DockerUtil utils = new DockerUtil(CONFIG.dockerClient);
-
-            utils.buildImageFromFolder(new File("../executor"), "logstash-executor");
-        }
-    }
-
     public static final MesosClusterConfig CONFIG = MesosClusterConfig.builder()
             // Note: Logstash-mesos uses container discovery, and mesos-local runs all
             // the executors in the same docker host. So it is safest to just use 1 slave for now..
             .numberOfSlaves(1)
+            .imagesToBuild(new MesosClusterConfig.ImageToBuild(new File("../executor"), "logstash-executor"))
             .slaveResources(new String[]{"ports(*):[9200-9200,9300-9300]"})
             .dockerInDockerImages(new String[]{"logstash-executor"}).build();
 
-    public static MesosCluster cluster = new MesosCluster(CONFIG);
-
     @ClassRule
-    public static RuleChain chain= RuleChain
-            .outerRule(new SmallInnerClass()).around(cluster);
-
+    public static MesosCluster cluster = new MesosCluster(CONFIG);
 
     @BeforeClass
     public static void startScheduler() {
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                new Scheduler(cluster.getMesosMasterURL(), "logstash-executor").run();
-            }
+        Thread t = new Thread(() -> {
+            new Scheduler(cluster.getMesosMasterURL(), "logstash-executor").run();
         });
         t.setDaemon(true);
         t.start();
@@ -59,20 +41,18 @@ public class MessageTest {
 
     @Test
     public void schedulerStarts() throws Exception {
+        // TODO await startup
 
-        cluster.getStateInfo();
+        State state = cluster.getStateInfo();
+        Framework logstash = state.getFramework("logstash");
 
-        System.out.println("HELLO WORLD");
-        //Scheduler scheduler = new Scheduler(cluster.getMasterUrl(), "epeld/logstash-executor");
-        //scheduler.run();
-
-        //
+        Assert.assertNotNull(logstash);
     }
 
     @Test
     public void schedulerStarts2() throws Exception {
 
-        cluster.getStateInfo();
+        cluster.getStateInfoJSON();
 
         System.out.println("HELLO WORLD2");
         //Scheduler scheduler = new Scheduler(cluster.getMasterUrl(), "epeld/logstash-executor");
