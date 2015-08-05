ccd9a7c674f980493223c8ff513257738f053c80
Florian Grundig <flg@trifork.com>
2015-08-05 11:03:05 +0200

    Set executor docker image to version 0.0.2

--- logstash-commons/src/main/java/org/apache/mesos/logstash/common/LogstashConstants.java
+++ logstash-commons/src/main/java/org/apache/mesos/logstash/common/LogstashConstants.java
@@ -3,7 +3,7 @@ package org.apache.mesos.logstash.common;
 public interface LogstashConstants {
     String FRAMEWORK_NAME = "logstash";
     int FAILOVER_TIMEOUT = 300000;
-    String EXECUTOR_IMAGE_NAME = "mesos/logstash-executor";
+    String EXECUTOR_IMAGE_NAME = "mesos/logstash-executor:0.0.2";
     String NODE_NAME = "logstash.node";
     String TASK_NAME = "logstash.task";
     String TASK_DATE_FORMAT = "yyyyMMdd'T'HHmmss.SSS'Z'";
