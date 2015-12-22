16d9a19f7c64cc5d4b85368507bd1f1e252b9ee4
James Fisher <jhf@trifork.com>
2015-12-22 13:46:04 +0000

    wait for image before trying to use it

--- system-test/src/test/java/org/apache/mesos/logstash/systemtest/DeploymentSystemTest.java
+++ system-test/src/test/java/org/apache/mesos/logstash/systemtest/DeploymentSystemTest.java
@@ -145,7 +145,7 @@ public class DeploymentSystemTest {
         final String sysLogPort = "514";
         final String randomLogLine = "Hello " + RandomStringUtils.randomAlphanumeric(32);
 
-        dockerClient.pullImageCmd("ubuntu:15.10").exec(new PullImageResultCallback());
+        dockerClient.pullImageCmd("ubuntu:15.10").exec(new PullImageResultCallback()).awaitSuccess();
         final String logstashSlave = dockerClient.listContainersCmd().withSince(cluster.getSlaves()[0].getContainerId()).exec().stream().filter(container -> container.getImage().endsWith("/logstash-executor:latest")).findFirst().map(Container::getId).orElseThrow(() -> new RuntimeException("Unable to find logstash container"));
         await().atMost(1, TimeUnit.MINUTES).pollDelay(1, TimeUnit.SECONDS).until(() -> {
             final CreateContainerResponse loggerContainer = dockerClient.createContainerCmd("ubuntu:15.10").withLinks(new Link(logstashSlave, "logstash")).withCmd("logger", "--server=logstash", "--port=" + sysLogPort, "--udp", "--rfc3164", randomLogLine).exec();
