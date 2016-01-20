bec0762467e92b06c0a04d59403a9f75c4c5faf8
Viktor Sadovnikov <viktor@jv-ration.com>
2016-01-20 13:37:08 +0100

    making docker image and its version configurable

--- logstash-scheduler/src/main/java/org/apache/mesos/logstash/config/LogstashConfig.java
+++ logstash-scheduler/src/main/java/org/apache/mesos/logstash/config/LogstashConfig.java
@@ -9,8 +9,12 @@ import java.util.Optional;
 @Component
 @ConfigurationProperties(prefix = "logstash")
 public class LogstashConfig {
+
     private int heapSize = 64;
     private Optional<URL> elasticsearchUrl = Optional.empty();
+
+    private String executorImage = null;
+    private String executorVersion = null;
 
     public int getHeapSize() {
         return heapSize;
@@ -26,5 +30,21 @@ public class LogstashConfig {
 
     public void setElasticsearchUrl(Optional<URL> elasticsearchUrl) {
         this.elasticsearchUrl = elasticsearchUrl;
+    }
+
+    public String getExecutorImage() {
+        return executorImage;
+    }
+
+    public void setExecutorImage(String executorImage) {
+        this.executorImage = executorImage;
+    }
+
+    public String getExecutorVersion() {
+        return executorVersion;
+    }
+
+    public void setExecutorVersion(String executorVersion) {
+        this.executorVersion = executorVersion;
     }
 }
