8119e5c066b8b0ba2f285211480c8a93a16db4f3
James Fisher <jhf@trifork.com>
2016-01-28 15:27:52 +0000

    remove unused import

--- logstash-scheduler/src/main/java/org/apache/mesos/logstash/scheduler/OfferStrategy.java
+++ logstash-scheduler/src/main/java/org/apache/mesos/logstash/scheduler/OfferStrategy.java
@@ -12,7 +12,6 @@ import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
